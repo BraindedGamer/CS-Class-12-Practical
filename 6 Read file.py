c52a93e107cb6e167d7a91079af66d9d1522c4e4
Yuvraj <62332805+BraindedGamer@users.noreply.github.com>
2021-03-25 13:16:40 +0530

    Out for printing

--- 6 Read file.py
+++ 6 Read file.py
@@ -1,6 +1,5 @@
 # Program to read content from a file and print it with spaces replaced with # symbol
-file = for i in range (n-2):
-    first,second = second,first+second # opening the file
+file = open('Text file') # opening the file
 content = file.read() # saving the content of the file into a variable
 words = content.split() # splitting the content by whitespaces using the builtin split() function
 
